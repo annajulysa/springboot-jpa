58e9cbb46f4dfa2badad19722ebf229149eaae19
Anna Almada <annasalmada@gmail.com>
2024-09-19 11:30:23 +0100

    project created

---
+++ src/main/java/com/annaalmada/course/CourseApplication.java
@@ -0,0 +1,14 @@
+package com.annaalmada.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// para configurar o projeto para ser um app spring boot
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
