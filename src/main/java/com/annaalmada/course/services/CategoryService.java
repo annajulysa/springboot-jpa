66236c8a3885f7dd43cc5f08d499dd359b785dd4
Anna Almada <annasalmada@gmail.com>
2024-09-19 13:18:02 +0100

    Category

---
+++ src/main/java/com/annaalmada/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.annaalmada.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.annaalmada.course.entities.Category;
+import com.annaalmada.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll() {
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return obj.get();
+	}
+}
