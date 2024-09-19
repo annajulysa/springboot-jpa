951c02d9bfb2e91eb07b6dfb6cf4bf68a09f934b
Anna Almada <annasalmada@gmail.com>
2024-09-19 11:42:40 +0100

    user entity reacre resource

---
+++ src/main/java/com/annaalmada/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.annaalmada.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.annaalmada.course.entities.User;
+
+// camada de recursos (rest controllers)
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	//endpoints
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Teste", "teste@gmail.com", "123456789", "12345");
+		return ResponseEntity.ok().body(user);
+	}
+}
